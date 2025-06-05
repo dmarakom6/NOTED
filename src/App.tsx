b60ab610093bb5b78a5cc1004b8f84112a06a78e
Dimitris Marakomihelakis <dmarakom@gmail.com>
2025-06-06 00:00:30 +0300

    experiment with routing

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/docs" element={<Docs />} />
+          <Route path="/NOTED/" element={<Index />} />
+          <Route path="/NOTED/docs" element={<Docs />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
