4dfd46ae02383a4a8bb5a0a9da0393030ae46a96
Dimitris Marakomihelakis <dmarakom@gmail.com>
2025-06-05 23:54:41 +0300

    resolve @

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
-  base: "/NOTED/"
+  base: "/NOTED/",
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
